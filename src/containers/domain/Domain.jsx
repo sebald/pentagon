e6ea8bdc31a9e8e40f31b999cd2d5b50472d41b9
Sebastian Sebald <sebastian.sebald@ptw.de>
2016-10-13 10:57:50 +0200

    :package: Selectors should be factories.

--- src/containers/domain/Domain.jsx
+++ src/containers/domain/Domain.jsx
@@ -5,14 +5,14 @@ import { getDomains } from './selectors';
 
 
 const mapStateToProps = createSelector(
-  getDomains,
+  getDomains(),
   domains => ({ domains })
 );
 
 export class DomainList extends React.Component {
   render() {
     const { domains } = this.props;
-    console.log(domains);
+    // console.log(domains);
     const items = Object.keys(domains).map(id => {
       return <li key={id}>{domains[id].label}</li>
     });
--- src/containers/domain/selectors.js
+++ src/containers/domain/selectors.js
@@ -1,1 +1,15 @@
-export const getDomains = state => state ? state.get('domains').toJS() : {};
+export const getDomains = () => state => state.get('domains').toJS();
+
+/**
+ * For alter reference:
+ * ===========================
+ *
+ * import { createSelector } from 'reselect';
+ *
+ * export const getStateProp = () => state => state.get('domains');
+ *
+ * export const getDomains = () => createSelector(
+ *    getStateProp(),
+ *    domains => domains.toJS()
+ * );
+ */
