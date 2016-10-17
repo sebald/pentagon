3056493b3847cd6dc62660586599cca5a321f580
Sebastian Sebald <sebastian.sebald@gmail.com>
2016-10-17 23:04:30 +0200

    :construction: Group List

--- src/containers/group/GroupList.jsx
+++ src/containers/group/GroupList.jsx
@@ -1,13 +1,35 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect'
 
-const GroupList = ({ pathname, domain_id }) => (
-  <div>
-    <h1>Groups for Domain { domain_id }</h1>
-  </div>
-)
+/** Helper */
+import { marginMedium } from '../../utils/space';
+
+/** Elements */
+import Helmet from 'react-helmet';
+import Link from 'react-router/Link';
+import Card from '../../components/card/Card';
+import { Row, Cell } from '../../components/table/Table';
+
+
+/** List */
+export class GroupList extends React.Component {
+  render() {
+    const { domain_id, pathname } = this.props;
+    return (
+      <div>
+        <Helmet title={`Groups in Domain ${domain_id}`}/>
+        <Card style={marginMedium}>
+          <h1>Groups in Domain {domain_id}</h1>
+        </Card>
+      </div>
+    );
+  }
+}
 
 GroupList.propTypes = {
-  pathname: PropTypes.string.isRequired
+  pathname: PropTypes.string.isRequired,
+  domain_id: PropTypes.string.isRequired
 }
 
 export default GroupList;
